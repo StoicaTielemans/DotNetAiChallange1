c91199867869e66e4d9a83635577ea99d1c6ffab
StoicaTielemans <r0983518@student.thomasmore.be>
2025-10-19 16:40:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DotNetAiChallange1
---
+++ TabTogether/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TabTogether
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
